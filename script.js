386e0966cf92f2f880c081cb6deb6b707cca1636
Korvah Harper Tarnue <103341154+2010601151@users.noreply.github.com>
2024-12-18 02:33:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Contact Form Submission
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for contacting us!');
+});
+
+// Appointment Form Submission
+document.getElementById('appointment-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Your appointment has been booked successfully!');
+});
